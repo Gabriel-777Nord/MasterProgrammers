bdec69986b2b2c5a0b7e961c0a3864fbac366b75
Gabriel <Gabbe.the.nord@gmail.com>
2021-05-28 11:13:04 +0200

    Test fungerar typ nu

--- MangkampAssignment/src/test/java/stepDefinitions/StepDefinitions.java
+++ MangkampAssignment/src/test/java/stepDefinitions/StepDefinitions.java
@@ -7,87 +7,72 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class StepDefinitions {
-	//TEST 2021-04-30
-		String input = "";
-		double result;
-	
-		@Given("I am a sports administrator")
-		public void i_am_a_sports_administrator() {
-		    System.out.println("First");
-		}
-		
-		@When("I register an {string}")
-		public void i_register_an(String string) {
-			System.out.println("Second");
-			Scanner scan = new Scanner(System.in);
+public String adminName;
+public String thlon;
+	@Given("I am a sports administrator")
+	public void i_am_a_sports_administrator() {
+		System.out.println("first");
+	}
 
-			System.out.print("How many participants? ");
-			int numParticipants = scan.nextInt();
-			Participants[] newParticipants = new Participants[numParticipants];
+	@Given("I want to be welcomed")
+	public void i_want_to_be_welcomed() {
+		System.out.println("Please enter the name of the administrator and press enter to confirm.");
+		adminName = "Mupp";
+	}
 
-			for (int i = 0; i < numParticipants; i++) {
-				System.out.print("Name the participants " + (i + 1) + ": ");
-				String name = scan.next();
-				Participants.Participants(name);
-				System.out.println(newParticipants [i]);
-				}
-			
-			//input = name1;
-			//input av namn på atlet
-		}
-		
-		public boolean isTrack = true;
-		
-		@When("I also register their {string}")
-		public void i_also_register_their(String string) {
-		//sporten if-sats bra
-			if (string.equals("track")){
-				isTrack = true;
-			}
-			if (string.equals("field")){
-				//something
-				isTrack = false;
-			}
-		}
+	@When("I click next")
+	public void i_click_next() {
+		System.out.println("third");
+	}
 
-		@When("I register their {int}")
-		public void i_register_their(Integer int1) {
-			System.out.println(newParticipants [1]);
-			//ekvation för B-P (7-kamp)!!
-			if(isTrack = true) {
-			double A = 25.4347;
-			double B = 18;
-			double C = 1.81;
-			double x = B-int1;
-			double i = Math.pow(x, C);
-			double resultekv = i*A;
-			int result = (int)resultekv;
-			System.out.println(result);
-			}
-			
-			// 7-kamp!!!
-			if (isTrack = false) {
-				double A = 1.84523;
-				double B = 75;
-				double C = 1.348;
-				double x = int1 - B;
-				double i = Math.pow(x, C);
-				double resultekv = i*A;
-				int result = (int)resultekv;
-				System.out.println(result);
-			}
-			
-		//	result = Math.pow(1.81, (25.4347(18-10)));
-			//Skriver in poängen från grenen
-		}
-		
-		@Then("I want to see the total score")
-		public void i_want_to_see_the_total_score() {
-			System.out.println("First");
-			
-			//Int Poäng = (A(B-P)^C)
-			//Uträkning sker från sparade värden och vi får poängen sparade
-		}
-		//metoder
-		
+	@Then("I see it welcoming me")
+	public void i_see_it_welcoming_me() {
+		System.out.println("Welcome " + adminName + " to the athletics score calculator.");
+	}
+
+	@Given("I am a administrator")
+	public void i_am_a_administrator() {
+		System.out.println("first");
+	}
+
+	@Given("I select hepathlon")
+	public void i_select_hepathlon() {
+		System.out.println("Press 1 for Decathlon or 2 for Heptathlon and press enter to confirm.");
+	}
+
+	@Given("I enter {int} of contestants")
+	public void i_enter_of_contestants(Integer int1) {
+		System.out.println("Please enter the number of competitors (1-40) and press enter to confirm.");
+	}
+
+	@When("I press next")
+	public void i_press_next() {
+		System.out.println("fourth");
+	}
+
+	@Then("it saves whitch hepathlon and how {int} contestants.")
+	public void it_saves_whitch_hepathlon_and_how_contestants(Integer int1) {
+		System.out.println("fith");
+	}
+
+	@Given("I am event administrator")
+	public void i_am_event_administrator() {
+		System.out.println("first");
+	}
+
+	@Given("I enter contestants name and nationality")
+	public void i_enter_contestants_name_and_nationality() {
+		System.out.println("Enter the name of competitor x, e.g Davud Mastsson and press enter to confirm.");//x = numret på deltagaren
+	}
+
+	@When("I have done that")
+	public void i_have_done_that(Integer int1) {
+		System.out.println("third");
+	}
+
+	@Then("I verify name and nationality")
+	public void i_verify_name_and_nationality_verifierar() {
+		System.out.println("Please enter the nationality for competitor x, e.g. SWE, and press enter to confirm.");//x = numret på deltagaren
+	}
+
 }
--- MangkampAssignment/src/test/resources/Features/scoring.feature
+++ MangkampAssignment/src/test/resources/Features/scoring.feature
@@ -1,17 +1,17 @@
-Feature: Welcome message
-I am a sports administrator and I want to be welcomed.
+#Feature: Welcome message
+#I am a sports administrator and I want to be welcomed.
 
-  Scenario: Administrator
-  Given I am a sports administrator
-  And <I> want to be welcomed
-  When I click next
-  Then I see it welcoming me
-     
- Feature: Initiation. #hur många + vilken kamp
- As a sportsadministrator I want to choose competition and number of contestants.
+#  Scenario: Administrator
+#  Given I am a sports administrator
+#  And I want to be welcomed
+#  When I click next
+#  Then I see it welcoming me
+    
+Feature: Initiation. #hur många + vilken kamp
+As a sportsadministrator I want to choose competition and number of contestants.
  
  		Scenario Outline: Setup succesfull
- 		Given I am a sports administrator
+ 		Given I am a administrator
  		And I select <competition>
  		And  I enter <number> of contestants
  		When I press next 
@@ -24,28 +24,11 @@ I am a sports administrator and I want to be welcomed.
 		| decathlon   |   1 	 |
 		| decathlon   |  40 	 |
 		
-		Scenario Outline: Setup less succesfull
- 		Given I am a sports administrator
- 		And I select <competition> 
- 		And  I enter <number> of contestants
- 		When I press next 
- 		Then it gives "<error message>"
- 		
-		Examples:
-		| competition | number | error message		 |
-		|	hepathlon   |   0 	 | minst en          |
-		|	hepathlon   |  41 	 | max 40 din potatis|
-		| decathlon   |   0 	 | minst en          |
-		| decathlon   |  41 	 | max 40 din potatis|
-		
-Feature: Register Contestant. #namn/var osv
-As a sportsadministrator I want to register contestant information so I can have a proper competition.
-	
-		Scenario Outline: Succesfull registration
-		Given I am a hurdidurr
-		And osv
-#Gör scenario på samma sätt. 
- 
-Feature: Overview. #deltagarlista	
-AS a sportadministrator I want to see all contestants information to get an overview over the competition.
-#Gör scenario på samma sätt.
+#Feature: Information #namn + nationalitet
+#Hejsan hoppsan
+
+#			Scenario: Who
+#		  Given I am event administrator
+#			And I enter contestants name and nationality
+#		  When I have done that 			#slumpar fram 40 
+#			Then I verify name and nationality  #verifierar
